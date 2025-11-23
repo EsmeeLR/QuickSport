53f7a253868bca2cc536fb1bc2dc5e04938965be
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-11-23 14:43:32 -0600

    Función para dividir el arreglo

--- QuickSort.java
+++ QuickSort.java
@@ -4,10 +4,25 @@ public class QuickSort {
     public static void quickSort(int[] arr, int inicio, int fin) {
         if (inicio < fin) {
             int indicePivote = particion(arr, inicio, fin);
-            quickSort(arr, inicio, indicePivote - 1); // ordenar la parte izquierda
-            quickSort(arr, indicePivote + 1, fin);   // ordena la  parte derecha
+            quickSort(arr, inicio, indicePivote - 1); // ordenar parte izquierda
+            quickSort(arr, indicePivote + 1, fin);   // ordenar parte derecha
         }
     }
 
+    // Función para dividir el arreglo según el pivote
+    private static int particion(int[] arr, int inicio, int fin) {
+        int pivote = arr[fin]; // último elemento como pivote
+        int i = inicio - 1;
 
+        for (int j = inicio; j < fin; j++) {
+            if (arr[j] <= pivote) {
+                i++;
+                // intercambio
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+    }
 }
