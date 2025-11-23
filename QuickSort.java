480304b723de1375b8df7b64cd90d913f993c386
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-11-23 15:01:46 -0600

    Se agregó el archivo de Testing

--- QuickSort.java
+++ QuickSort.java
@@ -1,15 +1,16 @@
+
 public class QuickSort {
 
     // Método principal de QuickSort
     public static void quickSort(int[] arr, int inicio, int fin) {
         if (inicio < fin) {
             int indicePivote = particion(arr, inicio, fin);
-            quickSort(arr, inicio, indicePivote - 1); // ordenar parte izquierda
-            quickSort(arr, indicePivote + 1, fin);   // ordenar parte derecha
+            quickSort(arr, inicio, indicePivote - 1);
+            quickSort(arr, indicePivote + 1, fin);  
         }
     }
 
-    // Función para dividir el arreglo según el pivote
+    // Método para dividir el arreglo según el pivote
     private static int particion(int[] arr, int inicio, int fin) {
         int pivote = arr[fin]; // último elemento como pivote
         int i = inicio - 1;
@@ -17,12 +18,26 @@ public class QuickSort {
         for (int j = inicio; j < fin; j++) {
             if (arr[j] <= pivote) {
                 i++;
-                // intercambio
+                // aquí se hace un intercambio
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
 
+        // colocar el pivote en su posición correcta
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[fin];
+        arr[fin] = temp;
+
+        return i + 1;
+    }
+
+    // Método para imprimir el arreglo
+    public static void imprimirArreglo(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
